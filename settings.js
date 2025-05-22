584f7c6b6f1490fcd15e2314b0fa633f5f855bd4
wanii398 <juniorwanii48@gmail.com>
2025-05-22 21:52:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'junior', // Your name
-  ownerNumber: '254108216737', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254768488032', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
